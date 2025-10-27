e1a89deed73d17186489bb99e961500c62717573
dialni <dini@itu.dk>
2025-10-27 17:07:54 +0100

    updated structure together with the boys, now we have a simple bidirectional stream working

--- client/client.go
+++ client/client.go
@@ -1,5 +1,59 @@
-package client
+package main
+
+import (
+	p "Chit-Chat/proto"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func ListenForMessages(stream grpc.BidiStreamingClient[p.Message, p.Message]) {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return
+		} else if err != nil {
+			log.Fatalf("failed to receive a message: %v", err)
+		}
+		fmt.Printf("%s says: %s\n", "Server", msg.Text)
+	}
+}
 
 func main() {
+	conn, err := grpc.NewClient(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
+	c := p.NewChatServiceClient(conn)
+	msg := p.Message{Text: "Hello from client!"}
+
+	resp, err := c.SayHello(context.Background(), &msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Response from server: ", resp.Text)
+	stream, err := c.ChatStream(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go ListenForMessages(stream)
+	fmt.Println("Enter a username: ")
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	uname := in.Text()
+
+	fmt.Printf("Welcome %s, say hello in the chat!\n", uname)
+	for in.Scan() {
+		text := in.Text()
+		stream.Send(&p.Message{Text: text})
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,5 +1,50 @@
-package server
+package main
+
+import (
+	p "Chit-Chat/proto"
+	"context"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	p.UnimplementedChatServiceServer
+}
+
+func (s *Server) SayHello(ctx context.Context, msg *p.Message) (*p.Message, error) {
+	log.Printf("Server Received: %s", msg)
+	return &p.Message{Text: "Hello from server!\n"}, nil
+}
+
+func (s *Server) ChatStream(stream p.ChatService_ChatStreamServer) error {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		log.Printf("Server Received: %s", msg.Text)
+		if err := stream.Send(&p.Message{Text: "Greetings, traveler!"}); err != nil {
+			return err
+		}
+	}
+}
 
 func main() {
+	s := &Server{}
+	grpcServer := grpc.NewServer()
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
+	log.Printf("server listening at %v", lis.Addr())
+	p.RegisterChatServiceServer(grpcServer, s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
